f56f4634c666676a859f4c720df15098a0b3dc13
panisara-bd <panisara.bnn@gmail.com>
2023-02-27 23:51:15 +0100

    Create the language-code db

---
+++ backend/scripts/language-codes.ts
@@ -0,0 +1,38 @@
+import fs from 'fs';
+
+type CountriesData = {
+  [key: string]: {
+    name: string;
+    native: string;
+    phone: number[];
+    continent?: string;
+    continents?: string[];
+    capital: string;
+    currency: string[];
+    languages: string[];
+  };
+};
+
+const getCountriesData = () => {
+  const res = fs.readFileSync('backend/db/countries-data.json').toString();
+  return JSON.parse(res);
+}
+
+const countriesData: CountriesData = getCountriesData();
+
+const languageCode = Object.keys(countriesData)
+  .map(countryCode => ({
+    countryCode,
+    languages: countriesData[countryCode].languages,
+  }))
+  .reduce(
+    (acc, curr) => ({
+      ...acc,
+      [curr.countryCode]: curr.languages,
+    }),
+    {}
+  );
+
+console.log(JSON.stringify(languageCode, null, 2));
+
+//run command: npx ts-node backend/scripts/language-codes.ts > backend/db/language-codes.json
